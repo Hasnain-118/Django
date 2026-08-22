441a94225126602995b9d8ecbcfff71d632f43c8
Muhammad Hasnain Iftikhar <mhasnain48776246@gmail.com>
2026-08-22 12:33:43 +0500

    New secret key

--- Hello/settings.py
+++ Hello/settings.py
@@ -23,13 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*q6op0j+9dv=@dgoqkcot@=$a$um#gl18ux9wdk75t0$fs&2&m'
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='127.0.0.1,localhost',
+    cast=lambda value: str(value).split(','),
+)
 
 
 # Application definition
