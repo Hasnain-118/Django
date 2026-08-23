e52e3d38ec7c9c3d12b0c1163a777fa905d18ea4
Muhammad Hasnain Iftikhar <mhasnain48776246@gmail.com>
2026-08-23 21:15:01 +0500

    jwt fix

--- Hello/settings.py
+++ Hello/settings.py
@@ -86,7 +86,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.google',
 ]
 
 # ====================
