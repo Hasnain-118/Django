31b97bc401526ebf81b74d28d5317cfd736832b7
Muhammad Hasnain Iftikhar <mhasnain48776246@gmail.com>
2026-08-25 16:27:17 +0500

    Contect table updated

--- home/models.py
+++ home/models.py
@@ -10,7 +10,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=30)
     email = models.CharField(max_length=35)
     phone = models.CharField(max_length=12)
-    message = models.TextField()
+    description = models.TextField()
     date = models.DateField()
 
     def __str__(self):
