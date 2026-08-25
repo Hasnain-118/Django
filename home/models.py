b55d299763a1223e14ae44d8b2aae90a15f74c72
Muhammad Hasnain Iftikhar <mhasnain48776246@gmail.com>
2026-08-25 16:23:48 +0500

    message field

--- home/models.py
+++ home/models.py
@@ -7,10 +7,10 @@ from django.contrib.auth.models import User
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=150)
-    email = models.CharField(max_length=150)
-    phone = models.CharField(max_length=20)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=35)
+    phone = models.CharField(max_length=12)
+    message = models.TextField()
     date = models.DateField()
 
     def __str__(self):
